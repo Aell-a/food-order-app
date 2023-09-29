dfe8e908fa2217945601dfd1e65fcc3b449992f9
Mordremoth <wantedlordd@gmail.com>
2023-09-29 16:57:27 +0300

    cart functionality added

--- src/components/Layout/HeaderButton.js
+++ src/components/Layout/HeaderButton.js
@@ -1,14 +1,42 @@
 import styles from "./HeaderButton.module.css";
 import CartIcon from "./CartIcon";
+import { useContext, useEffect, useState } from "react";
+import CartContext from "../../store/cart-context";
 
 const HeaderButton = (props) => {
+  const [btnHighlight, setBtnHighlight] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+
+  const numberOfItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  const btnStyles = `${styles.button} ${btnHighlight ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnHighlight(true);
+
+    const timer = setTimeout(() => {
+      setBtnHighlight(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnStyles} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>0</span>
+      <span className={styles.badge}>{numberOfItems}</span>
     </button>
   );
 };
